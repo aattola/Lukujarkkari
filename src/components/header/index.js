8a6738ed75830236a467133b4cce8bf5b1acfb2f
JEFFe <andrejeff.leevi@gmail.com>
2021-04-15 20:13:23 +0300

    Link to jeffe.co

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,7 +5,7 @@ import Logo from '../../assets/Logo4.png';
 
 const Header = () => (
 	<header class={style.header} style={{display: "grid", alignItems: "center", background: "white", borderBottom: "1px solid #00000055"}}>
-    <img src={Logo} style={{width: "38px", margin: "0px 20px"}} />
+    <img onClick={() => window.location = "https://jeffe.co"} src={Logo} style={{width: "38px", margin: "0px 20px", cursor: "pointer"}} />
 		{/* <nav>
 			<Link activeClassName={style.active} href="/">Home</Link>
 			<Link activeClassName={style.active} href="/profile">Me</Link>
